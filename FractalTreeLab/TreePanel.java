35367d33d15afd8387f291493494d310f9108946
irbarnard <IRBARNARD@student.naperville203.org>
2015-03-24 11:25:23 -0500

    tree
    eert

---
+++ FractalTreeLab/TreePanel.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class TreePanel
+{
+    private final int PANEL_WIDTH = 400;
+    private final int PANEL_HEIGHT = 400;
+
+    private final double SQ = Math.sqrt(3.0) / 6;
+
+    private final int TOPX = 200, TOPY = 20;
+    private final int LEFTX = 60, LEFTY = 300;
+    private final int RIGHTX = 340, RIGHTY = 300;
+
+    private int current; //current order
+    /**
+     * Default constructor for objects of class TreePanel
+     */
+    public TreePanel()
+    {
+       current = currentOrder;
+      setBackground (Color.black);
+      setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
---
+++ FractalTreeLab/TreeViewer.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class TreeViewer
+{
+    private final int WIDTH = 400;
+    private final int HEIGHT = 500;
+    
+    private JLabel titleLabel, orderLabel;
+    private TreePanel drawing;
+    private JPanel panel, tools;
+    private JFrame frame;
+
+    public static void main(String[] args)
+    {
+        TreeViewer viewer = new TreeViewer();
+    }
+
+    public TreeViewer()
+    {
+        orderLabel = new JLabel ("Order: 1");
+        orderLabel.setForeground (Color.black);
+
+        drawing = new TreePanel (1);
+
+        panel = new JPanel();
+        panel.add (tools);
+        panel.add (drawing);
+
+        frame = new JFrame();
+        frame.setTitle("Knoch Snowflake");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+}
